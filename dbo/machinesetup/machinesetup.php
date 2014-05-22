81b73b80827ffd2b06f616d4ab915f6495e8d3ce
esp <esp@phiorion.com>
2014-05-22 17:37:12 +0800

    finish machine layout

--- dbo/machinesetup/machinesetup.php
+++ dbo/machinesetup/machinesetup.php
@@ -1,6 +1,20 @@
 <?php
 require(dirname(__FILE__).DIRECTORY_SEPARATOR.'machinesetup.conf.php');
 
+function fiveinputinarow_new($col, $colVal, $data=array(), $html=null){
+	$colname = $col;
+	
+	$inputname = substr($colname,1,strlen($colname)-1);
+	// get the input name
+	$html = '<div id="'.'dbo_machinesetup_new_'.$inputname.'"  inputtype="text" inputtypesize="">';
+
+	for($i=1;$i<=5;$i++){
+		$html .= '<input type="text" class="fiveinarow" name="'.'dbo_machinesetup_new_'.$inputname.$i.'" id="'.'dbo_machinesetup_new_'.$inputname.$i.'" value="">';
+	}
+	$html .="</div>";
+
+	return $html;
+}
 # customization
 function dbo_machinesetup_customize(&$dbo){
 }
