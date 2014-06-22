76155bf2560e51c3bf898a401893157cc3b6abf9
ysyow <lampard7824@gmail.com>
2014-06-22 15:50:34 +0800

    asd

---
+++ lang/EN-US/pmtask.lang.php
@@ -0,0 +1,18 @@
+<?php
+/*Last Modified by uat_supervisor1 on 22-06-2014 14:40:58*/
+$translate = array (
+  'Start Event' => 'Start Event',
+  'New Job' => 'New Job',
+  'User Activity' => 'User Activity',
+  'Pending acceptance' => 'Pending acceptance',
+  'Artwork job' => 'Artwork job',
+  'Task' => 'Task',
+  'Requirement verification' => 'Requirement verification',
+  'Rework in progress' => 'Rework in progress',
+  'Intermediate Event' => 'Intermediate Event',
+  'Acknowledge Job' => 'Acknowledge Job',
+  'Pull Back' => 'Pull Back',
+  'Work in progress ' => 'Work in progress ',
+  'Reassign Artist' => 'Reassign Artist',
+);
+?>
