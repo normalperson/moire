65dd7eaf02cdb33929dfdb0bd4475c98bd93a07e
esp <esp@phiorion.com>
2014-06-26 10:49:58 +0800

    update license file

--- lang/EN-US/license.lang.php
+++ lang/EN-US/license.lang.php
@@ -1,11 +1,6 @@
 <?php
-<<<<<<< HEAD
-/*Last Modified by admin on 25-06-2014 10:55:32*/
-=======
-/*Last Modified by admin2 on 25-06-2014 14:01:06*/
->>>>>>> 7f2c2abb289fe49fee35152a69c516c8f02bd234
+/*Last Modified by Anonymous on 26-06-2014 10:43:54*/
 $translate = array (
-  'Click Here' => 'Click Here',
   'License will expire in %d day(s)' => 'License will expire in %d day(s)',
 );
 ?>
