b8309a4a316fab27a3ef3728845913a8e0469426
esp <esp@phiorion.com>
2014-06-26 10:38:52 +0800

    commit local

--- lang/EN-US/license.lang.php
+++ lang/EN-US/license.lang.php
@@ -1,6 +1,7 @@
 <?php
-/*Last Modified by admin on 21-06-2014 18:07:29*/
+/*Last Modified by admin on 25-06-2014 10:55:32*/
 $translate = array (
   'Click Here' => 'Click Here',
+  'License will expire in %d day(s)' => 'License will expire in %d day(s)',
 );
 ?>
