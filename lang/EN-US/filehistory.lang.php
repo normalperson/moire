0318ac47a78296828590c43684cc38da512d6582
esp <esp@phiorion.com>
2014-07-03 22:20:13 +0800

    added filehistory translation

---
+++ lang/EN-US/filehistory.lang.php
@@ -0,0 +1,11 @@
+<?php
+/*Last Modified by esp on 03-07-2014 22:19:22*/
+$translate = array (
+  'Created date' => 'Created date',
+  'Created by' => 'Created by',
+  'File name' => 'File name',
+  'File ext' => 'File ext',
+  'File size' => 'File size',
+  'Download' => 'Download',
+);
+?>
---
+++ lang/ZH-CN/filehistory.lang.php
@@ -0,0 +1,11 @@
+<?php
+/*Last Modified by esp on 03-07-2014 22:19:22*/
+$translate = array (
+  'Created date' => '新建日期',
+  'Created by' => '新建者',
+  'File name' => '文件名字',
+  'File ext' => '文件擴展名',
+  'File size' => '文件大小',
+  'Download' => '下载',
+);
+?>
