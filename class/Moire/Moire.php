d75d3c60a331a2de757cd6b7898fee715fa3da4c
fong <fong@phiorion.com>
2014-07-15 20:46:53 +0800

    currencysetup audit

--- class/Moire/Moire.php
+++ class/Moire/Moire.php
@@ -1,6 +1,14 @@
 <?php
 require_once(dirname(__FILE__).'/../../init.inc.php');
+/*
+Moire class will handle invoicing, payment and billing here because moire is not using standard method.
+*/
 class Moire{
+	function __construct(){
+	}
 	
+	// generate invoice by job (mjobsheet)
+	function invoice_generate($jsid){
+	}
 }
 ?>
--- dbo/currencysetup/currencysetup.php
+++ dbo/currencysetup/currencysetup.php
@@ -3,8 +3,18 @@ require(dirname(__FILE__).DIRECTORY_SEPARATOR.'currencysetup.conf.php');
 
 # customization
 function dbo_currencysetup_customize(&$dbo){
+	$dbo->editModifier = 'dbo_currencysetup_custom_edit';
 }
 
+function dbo_currencysetup_custom_edit($table, $cols, $wheres){
+	global $DB;
+	$ret = array();
+	$ok = $DB->doUpdateAudit($table, $cols, $wheres, array('cr_rate'));
+	if(!$ok){
+		$ret[] = $DB->lastError;
+	}
+	return $ret;
+}
 
 # final rendering
 $dbo->render();
