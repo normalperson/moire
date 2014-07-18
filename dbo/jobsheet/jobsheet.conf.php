369408898aeb350727c3a49ff2387484a7d95c05
esp <esp@phiorion.com>
2014-07-18 18:23:43 +0800

    fix the output problem

--- dbo/jobsheet/jobsheet.conf.php
+++ dbo/jobsheet/jobsheet.conf.php
@@ -21,7 +21,7 @@ select string_agg(jc_jclid::char,\', \' order by jc_jclid) jobcategory,jc_jsid f
 group by jc_jsid) a on js_id = a.jc_jsid
 left join (
 select string_agg(jo_outputcode,\', \' order by jo_id) joboutput,jo_jsid from mjoboutput
-group by jo_id) b on js_id = b.jo_jsid';
+group by jo_jsid) b on js_id = b.jo_jsid';
 $dbo->col = array('js_id', 'js_orgid', 'js_ctid', 'js_request_date', 'js_request_by', 'js_title', 'js_model', 'js_description', 'js_material_provided', 'js_color_1', 'js_color_2', 'js_color_3', 'js_color_4', 'js_color_5', 'js_angle_1', 'js_angle_2', 'js_angle_3', 'js_angle_4', 'js_angle_5', 'js_bleeding', 'js_bleeding_remark', 'js_distortion', 'js_distortion_value', 'js_diecut_ind', 'js_diecut_no', 'js_trapping_size', 'js_primcat', 'js_status', 'js_completiondate', 'js_assignto', 'js_carid', 'js_decision', 'js_width', 'js_height', 'js_requiretime', 'js_request_dateinmth', 'js_jobcolor', 'js_lpi', 'js_mcid', 'js_code', 'js_month_occur', 'js_price', 'js_outputtype', 'js_outputwidth', 'js_outputheight', 'js_qcchecked', 'js_currency', 'js_finalprice', 'js_rate', 'attachment', 'remark', 'info', 'filehistory', 'pmc_id', 'jobcategory', 'joboutput', '__map_mjobbarcode__jbc_jsid__', '__map_mjobbarcode__jbc_btcode__', '__map_mjobbarcode__jbc_value__');
 $dbo->colList = array('js_description', 'js_bleeding', 'js_bleeding_remark', 'js_distortion', 'js_distortion_value', 'js_diecut_ind', 'js_primcat');
 $dbo->colListEdit = array();
