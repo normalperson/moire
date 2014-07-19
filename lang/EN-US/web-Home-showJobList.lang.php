a38c5efe773b9f262d8a63daf0a13b715ad1e6e6
esp <esp@phiorion.com>
2014-07-19 12:02:11 +0800

    updated before merge with yow

---
+++ lang/EN-US/web-Home-showJobList.lang.php
@@ -0,0 +1,9 @@
+<?php
+/*Last Modified by uat_customer1 on 17-07-2014 00:38:30*/
+$translate = array (
+  'Create New Jobsheet' => 'Create New Jobsheet',
+  'New Jobsheet' => 'New Jobsheet',
+  'Print' => 'Print',
+  'New' => 'New',
+);
+?>
---
+++ lang/ZH-CN/Messaging.lang.php
@@ -0,0 +1,8 @@
+<?php
+/*Last Modified by uat_supervisor1 on 13-07-2014 02:21:39*/
+$translate = array (
+  'Messages' => 'Messages',
+  'NO MESSAGES' => 'NO MESSAGES',
+  'COMPOSE' => 'COMPOSE',
+);
+?>
